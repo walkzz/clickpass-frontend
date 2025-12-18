fcac05c58f408ec1b94fb01267e710bc4cacfdae
Pavel Toshinski <pavelce2000@hotmail.com>
2025-12-18 01:53:53 +0100

    Added basic structure to Toolbar.jsx

---
+++ src/components/Toolbar.jsx
@@ -0,0 +1,11 @@
+import Search from "./Search";
+import Dropdown from "./Dropdown";
+const Toolbar = () => {
+  return (
+    <div>
+      <Search />
+      <Dropdown />
+    </div> // example
+  );
+};
+export default Toolbar;
