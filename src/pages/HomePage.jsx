28b4503ac31695d309967b5be551f3939c3eff6f
Pavel Toshinski <pavelce2000@hotmail.com>
2025-12-18 02:01:20 +0100

    Imported components into HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,17 @@
+import Navbar from "../components/Navbar";
+import Toolbar from "../components/Toolbar";
+import EventList from "../components/EventList";
+import Assistant from "../components/Assistant";
+import Footer from "../components/Footer";
 const HomePage = () => {
   return (
-    <div>
-      HomePage 
-    </div> // test
+    <>
+      <Navbar />
+      <Toolbar />
+      <EventList />
+      <Assistant />
+      <Footer />
+    </>
   );
 };
 export default HomePage;
