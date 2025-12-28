2e9d4a76f66650b527d24509a190530e15a9b100
Pavel Toshinski <pavelce2000@hotmail.com>
2025-12-28 01:54:39 +0100

    Added routing to links

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import './App.css'
 import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import EventsPage from "./pages/EventsPage";
+import BlogsPage from "./pages/BlogsPage";
+import ContactPage from "./pages/ContactPage";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
-  return <HomePage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Navigate to="/home" replace />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/events" element={<EventsPage />} />
+        <Route path="/blogs" element={<BlogsPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
