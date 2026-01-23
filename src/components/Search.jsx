f8f6b827d21c963df968aeb5a32a10b2aba644d7
Pavel Toshinski <pavelce2000@hotmail.com>
2026-01-23 02:38:45 +0100

    Added search options to the dropdown component and added styles

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,18 +1,64 @@
-import '../styles/Search.css'
+import React, { useState } from 'react';
+import '../styles/Search.css';
 import Dropdown from './Dropdown';
+
 const Search = () => {
-  return <div className='search-bar'>
-    <div className='search-wrapper'>
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-        <circle cx="11" cy="11" r="8"></circle>
-        <path d="m21 21-4.3-4.3"></path>
-      </svg>
-      <input type="text" placeholder="Search by event name..." />
+  const categoryOptions = [
+    "All",
+    "Theater",
+    "Concert",
+    "Cinema",
+    "Opera"
+  ];
+
+  const locationOptions = [
+    "All",
+    "New York, NY",
+    "Los Angeles, CA",
+    "Chicago, IL",
+    "London, UK",
+    "New Orleans, LA",
+    "Boston, MA",
+    "Seattle, WA",
+    "Berlin, DE"
+  ];
+  const [category, setCategory] = useState("All");
+  const [location, setLocation] = useState("All");
+
+  return (
+    <div className='search-bar'>
+      <div className='search-wrapper'>
+        <svg
+          style={{ marginLeft: '1rem' }}
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <circle cx="11" cy="11" r="8"></circle>
+          <path d="m21 21-4.3-4.3"></path>
+        </svg>
+        <input type="text" placeholder="Search by event name..." />
+      </div>
+      <div className='dropdown-container'>
+        <Dropdown
+          options={categoryOptions}
+          selected={category}
+          onSelect={setCategory}
+        />
+        <Dropdown
+          options={locationOptions}
+          selected={location}
+          onSelect={setLocation}
+        />
+      </div>
     </div>
-    <div className='dropdown-container'>
-      <Dropdown />
-      <Dropdown />
-    </div>
-  </div>;
+  );
 };
+
 export default Search;
