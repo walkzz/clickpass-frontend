a71dd51600c7a8cfab22abbf8e62d6b4bdfa59f8
Pavel Toshinski <pavelce2000@hotmail.com>
2026-02-24 01:07:41 +0100

    Added profile dropdown option

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import '../styles/Navbar.css'
 import { NavLink } from "react-router-dom";
+import UserProfileDropdown from './ProfileDropDown';
 const Navbar = () => {
   return (
     <header className='header'>
@@ -18,23 +19,10 @@ const Navbar = () => {
             <NavLink className={"navigation-button"} to="/events">Events</NavLink>
             <NavLink className={"navigation-button"} to="/blogs">Blogs</NavLink>
             <NavLink className={"navigation-button"} to="/contact">Contact</NavLink>
+            {/* <NavLink className={"navigation-button"} to="/login">Login</NavLink>
+            <NavLink className={"navigation-button"} to="/register">Register</NavLink> */}
           </nav>
-          <div className="">
-            <a className="" href="/admin">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-circle-user-round h-6 w-6"><path d="M18 20a6 6 0 0 0-12 0"></path><circle cx="12" cy="10" r="4"></circle>
-                <circle cx="12" cy="12" r="10"></circle>
-              </svg>
-              {/* <span className="">Profile</span> */}
-            </a>
-            {/* <button className="" type="button" aria-haspopup="dialog" aria-expanded="false" aria-controls="radix-«R1ddb»" data-state="closed">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-menu h-6 w-6">
-                <line x1="4" x2="20" y1="12" y2="12"></line>
-                <line x1="4" x2="20" y1="6" y2="6"></line>
-                <line x1="4" x2="20" y1="18" y2="18"></line>
-              </svg>
-              <span className="">Open navigation menu</span>
-            </button> */}
-          </div>
+          <UserProfileDropdown />
         </div>
     </header>
   );
