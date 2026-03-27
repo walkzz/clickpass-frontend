42387a1e938705d4e02f951112ac47a516d331db
Pavel Toshinski <pavelce2000@hotmail.com>
2026-03-27 16:02:31 +0100

    Added new SVG to the blog management

--- src/components/ManageBlogsTab.jsx
+++ src/components/ManageBlogsTab.jsx
@@ -42,7 +42,9 @@ const ManageBlogsTab = () => {
               <td><span className="category-badge">{post.category}</span></td>
               <td>{new Date(post.createdAt).toLocaleDateString()}</td>
               <td style={{ textAlign: 'right' }}>
-                <button onClick={() => deletePost(post._id)} className="action-icon" style={{color: 'red'}}>🗑️</button>
+                <button onClick={() => deletePost(post._id)} className="action-icon" style={{color: 'red'}}>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash2 h-4 w-4"><path d="M3 6h18"></path><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"></path><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"></path><line x1="10" x2="10" y1="11" y2="17"></line><line x1="14" x2="14" y1="11" y2="17"></line></svg>
+                </button>
               </td>
             </tr>
           ))}
