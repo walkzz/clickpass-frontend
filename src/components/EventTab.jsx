c1552ebebbb4253c4085f2e05a1c81cee4b01f7d
Pavel Toshinski <pavelce2000@hotmail.com>
2026-03-27 15:36:30 +0100

    Fixed gap between svg and text, sepeare styles are needed to avoid overlapping

--- src/components/EventTab.jsx
+++ src/components/EventTab.jsx
@@ -94,8 +94,8 @@ const EventTab = () => {
       <div className="management-card">
         <div className="management-header">
           <h2 className="management-title">Event Management</h2>
-          <button onClick={handleCreateClick} className="btn-pink"> 
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-plus mr-2 h-4 w-4"><circle cx="12" cy="12" r="10"></circle><path d="M8 12h8"></path><path d="M12 8v8"></path></svg>
+          <button onClick={handleCreateClick} className="btn-pink btn-management-event"> 
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-plus mr-2 h-4 w-4"><circle cx="12" cy="12" r="10"></circle><path d="M8 12h8"></path><path d="M12 8v8"></path></svg>
             Create Event</button>
         </div>
 
